7db719767052a360a3736e28d8d1d88ef5482541
Mrunali <mrunaliurade26@gmail.com>
2025-08-03 09:00:26 +0530

    Reversing given string

--- ReverseString.java
+++ ReverseString.java
@@ -12,7 +12,8 @@ public class ReverseString {
             right--;
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
        // char[] s = {'H', 'E', 'L', 'L', 'O'};
         char[] s = {'M', 'R', 'U', 'N', 'A', 'L', 'I'};
         System.out.println("Original string : " + new String(s));
