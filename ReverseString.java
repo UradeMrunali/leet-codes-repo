6aacf862feba164ce0badb8d0022e7c98fc5aefb
Mrunali <mrunaliurade26@gmail.com>
2025-08-03 08:58:12 +0530

    Reversing given string

---
+++ ReverseString.java
@@ -0,0 +1,23 @@
+public class ReverseString {
+    public static void reverseString (char[] s){
+        int left = 0;
+        int right = s.length-1;
+
+        while (left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+       // char[] s = {'H', 'E', 'L', 'L', 'O'};
+        char[] s = {'M', 'R', 'U', 'N', 'A', 'L', 'I'};
+        System.out.println("Original string : " + new String(s));
+        reverseString(s);
+        System.out.println("Reversed string : " + new String(s));
+    }
+}
