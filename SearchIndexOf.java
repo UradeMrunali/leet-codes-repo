f7566ee1ee92ab4dd1e0c823d6ed5538351e85b4
Mrunali <mrunaliurade26@gmail.com>
2025-08-07 21:20:39 +0530

    Searching index of given element

--- SearchIndexOf.java
+++ SearchIndexOf.java
@@ -6,8 +6,6 @@ public class SearchIndexOf {
         ArrayList<Integer> numbers = new ArrayList<>();
         Collections.addAll(numbers, 4,5,8,3,9,7,10);
         int target = 9;
-
-
         int indexOf = numbers.indexOf(target);
         if(indexOf != -1){
             System.out.println("Index of the "+ target + " is: " + indexOf);
